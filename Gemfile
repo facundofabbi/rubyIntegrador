20a3adcd368a81788b578aef6a9a8a6729ea275c
root <facundo fabbi>
2022-11-24 19:32:42 -0300

    se creo el listado de usuarios demo

--- Gemfile
+++ Gemfile
@@ -6,11 +6,15 @@ ruby "2.7.1"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
 
+gem "rolify"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
 # Use mysql2 as the database for Active Record
 gem "mysql2"
+
+gem 'devise'#, git: 'git://github.com/plataformatec/devise.git'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_211620) do
+  create_table "roles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
+  end
+
+  create_table "usuarios", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "nombre", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nombre"], name: "index_usuarios_on_nombre", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
+  end
+
+  create_table "usuarios_roles", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "usuario_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_usuarios_roles_on_role_id"
+    t.index ["usuario_id", "role_id"], name: "index_usuarios_roles_on_usuario_id_and_role_id"
+    t.index ["usuario_id"], name: "index_usuarios_roles_on_usuario_id"
+  end
+
 end
