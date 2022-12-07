7c4c5d2c3410c785703363a8112ba62cf9dbb6c4
facundofabbi <62101931+facundofabbi@users.noreply.github.com>
2022-12-07 11:26:41 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,24 @@ Sucursal.create(:nombre => "Villa Elisa", :direccion => "489 y 13", :telefono =>
 Sucursal.create(:nombre => "Gonnet", :direccion => "511 y centenario", :telefono => "785236")
 
 Usuario.create(:nombre => "Administrador principal", :email => "admin@admin", :password => "123456", :password_confirmation => "123456", :role => "Admin")
-Usuario.create(:nombre => "Personal bancario", :email => "personal@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "1")
+Usuario.create(:nombre => "Administrador B", :email => "adminB@admin", :password => "123456", :password_confirmation => "123456", :role => "Admin")
 
-Usuario.create(:nombre => "Cliente", :email => "cliente@cliente", :password => "123456", :password_confirmation => "123456")
-Usuario.create(:nombre => "ClienteB", :email => "clienteB@cliente", :password => "123456", :password_confirmation => "123456")
-Usuario.create(:nombre => "ClienteC", :email => "clienteC@cliente", :password => "123456", :password_confirmation => "123456")
-Usuario.create(:nombre => "ClienteD", :email => "clienteD@cliente", :password => "123456", :password_confirmation => "123456")
-Usuario.create(:nombre => "ClienteE", :email => "clienteE@cliente", :password => "123456", :password_confirmation => "123456")
+Usuario.create(:nombre => "Personal bancario", :email => "personal@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "1")
+Usuario.create(:nombre => "Personal bancario 2", :email => "personalB@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "1")
+
+Usuario.create(:nombre => "Personal bancario 3", :email => "personalC@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "2")
+Usuario.create(:nombre => "Personal bancario 4", :email => "personalD@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "2")
+
+Usuario.create(:nombre => "Personal bancario 5", :email => "personalE@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "3")
+Usuario.create(:nombre => "Personal bancario 6", :email => "personalF@personal", :password => "123456", :password_confirmation => "123456", :role => "Personal Bancario", :sucursal_id => "3")
+
+
+
+Usuario.create(:nombre => "Cliente", :email => "cliente@cliente", :password => "123456", :password_confirmation => "123456") #9
+Usuario.create(:nombre => "ClienteB", :email => "clienteB@cliente", :password => "123456", :password_confirmation => "123456") #10
+Usuario.create(:nombre => "ClienteC", :email => "clienteC@cliente", :password => "123456", :password_confirmation => "123456") #11
+Usuario.create(:nombre => "ClienteD", :email => "clienteD@cliente", :password => "123456", :password_confirmation => "123456") #12
+Usuario.create(:nombre => "ClienteE", :email => "clienteE@cliente", :password => "123456", :password_confirmation => "123456") #13
 
 
 
@@ -39,3 +50,17 @@ Horario.create(:dia => "Martes", :hora_inicio => "11:00", :hora_fin => "17:00", 
 Horario.create(:dia => "Miercoles", :hora_inicio => "06:00", :hora_fin => "16:00", :sucursal_id => 3)
 Horario.create(:dia => "Jueves", :hora_inicio => "19:00", :hora_fin => "20:00", :sucursal_id => 3)
 Horario.create(:dia => "Viernes", :hora_inicio => "11:00", :hora_fin => "16:00", :sucursal_id => 3)
+
+Turno.create(:fecha => "02-01-2023", :hora => "11:00", :motivo => "Prestamos de $700", :estado => "Pendiente", :sucursal_id => 1, :cliente_id => 9)
+Turno.create(:fecha => "02-01-2023", :hora => "12:00", :motivo => "Consulta bancaria por motivos de cobros", :estado => "Pendiente", :sucursal_id => 2, :cliente_id => 9)
+Turno.create(:fecha => "04-01-2023", :hora => "08:00", :motivo => "Sin Motivo", :estado => "Pendiente", :sucursal_id => 3, :cliente_id => 9)
+
+Turno.create(:fecha => "02-01-2023", :hora => "11:00", :motivo => "Prestamo de casa", :estado => "Pendiente", :sucursal_id => 1, :cliente_id => 10)
+Turno.create(:fecha => "02-01-2023", :hora => "12:00", :motivo => "Prestamo de un inflable", :estado => "Pendiente", :sucursal_id => 2, :cliente_id => 10)
+Turno.create(:fecha => "04-01-2023", :hora => "08:00", :motivo => "Prestamo para poder comprar una ventana", :estado => "Pendiente", :sucursal_id => 3, :cliente_id => 10)
+
+Turno.create(:fecha => "03-01-2023", :hora => "11:00", :motivo => "Preguntas por alta de cuenta", :estado => "Atendido", :sucursal_id => 1, :cliente_id => 9, :personal_id => 3)
+Turno.create(:fecha => "03-01-2023", :hora => "11:00", :motivo => "Quiero cerar mi cuenta", :estado => "Atendido", :sucursal_id => 2, :cliente_id => 9, :personal_id => 3)
+
+Turno.create(:fecha => "03-01-2023", :hora => "11:00", :motivo => "Dudas de un prestamo que solicite", :estado => "Atendido", :sucursal_id => 1, :cliente_id => 10, :personal_id => 3)
+Turno.create(:fecha => "03-01-2023", :hora => "11:00", :motivo => "Fui porque tenia un conocido", :estado => "Atendido", :sucursal_id => 2, :cliente_id => 10, :personal_id => 3)
